556e8e0f659be816463bdf54bf30aeb67cee88e8
mrodriguezcartabia <mrodriguezcartabia@gmail.com>
2026-01-07 19:25:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -92,8 +92,11 @@ function setLanguage(lang) {
         renderMathInElement(document.body, {
             delimiters: [
                 {left: '$$', right: '$$', display: true},
-                {left: '$', right: '$', display: false}
-            ]
+                {left: '$', right: '$', display: false},
+                {left: '\\(', right: '\\)', display: false},
+                {left: '\\[', right: '\\]', display: true}
+            ],
+            throwOnError: false
         });
     }
 
